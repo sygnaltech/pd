faa354575f602b15dde69463415c9838da740243
Michael Wells <info@sygnal.com>
2024-09-27 00:58:46 +0000

    Posthog added

--- src/site.ts
+++ src/site.ts
@@ -4,6 +4,7 @@
  */
 
 import { IRouteHandler, Page } from "@sygnal/sse";
+import posthog from 'posthog-js'
 // import { loadEngineCSS } from "./engine/core";
 
 // import gsap from 'gsap'; 
@@ -18,6 +19,8 @@ export class Site implements IRouteHandler {
 
     Page.loadEngineCSS("site.css"); 
    
+    posthog.init('phc_XNXwsF5o7snDKAZVngbWLupKigbqv16MBNOC8UoWKE', { api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only' })
+
   }
 
   exec() {
